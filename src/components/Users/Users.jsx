e47b8526152c0361c1c500c469a9d4fbf3e7bf0c
slimnation23 <slimnation2@gmail.com>
2020-08-24 16:32:04 +0300

    class Component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,29 +3,28 @@ import s from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
-let Users = (props) => {
 
-    let getUsers = () => 
-    {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    props.setUsers(response.data.items)
-                });
-        }
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
     }
 
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
+    render() {
+        return <div>
         {
-            props.users.map(u =>
+            this.props.users.map(u =>
                 <div key={u.id} className={s.info}>
                     <div className={s.infoLeft}>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.image} />
                         <div>
                             {u.followed
-                                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                                ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
                         </div>
                     </div>
                     <div className={s.infoRight}>
@@ -41,6 +40,7 @@ let Users = (props) => {
                 </div>)
         }
     </div>
+    }
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Users from './Users'
+import Users from './Users';
 import { connect } from 'react-redux';
 import { followAC, unfollowAC, setUsersAC } from './../../redux/usersReducer';
 
